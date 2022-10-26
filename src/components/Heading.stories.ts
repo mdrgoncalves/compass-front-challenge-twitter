4f5352946e76626df68eca0d2780471db6bd81d2
mdrgoncalves <mdr.rgon@gmail.com>
2022-10-26 17:52:09 -0300

    feat: heading component

---
+++ src/components/Heading.stories.ts
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/html';
+import { createHeading, HeadingProps } from './Heading';
+
+export default {
+    title: 'Components/Heading',
+    argTypes: {
+        level: {
+            control: { type: 'select' },
+            options: [1, 2],
+        },
+        text: { control: 'text' },
+    },
+} as Meta;
+
+const Template: Story<HeadingProps> = (args) => {
+    return createHeading(args);
+}
+
+export const Heading1 = Template.bind({});
+Heading1.args = {
+    level: 1,
+    text: 'Heading 1',
+};
+
+export const Heading2 = Template.bind({});
+Heading2.args = {
+    level: 2,
+    text: 'Heading 2',
+};
+
---
+++ src/components/Heading.ts
@@ -0,0 +1,19 @@
+import '/public/assets/styles/main.css';
+
+export interface HeadingProps {
+    level: 1 | 2;
+    text: string;
+}
+
+export const createHeading = ({
+    level,
+    text
+}: HeadingProps) => {
+    
+    const heading = document.createElement(`h${level}`);
+    heading.classList.add(`heading-${level}`);
+    heading.innerHTML = text;
+
+    return heading;
+};
+    
