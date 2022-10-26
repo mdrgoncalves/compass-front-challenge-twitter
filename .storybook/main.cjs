734c9aa31fadffe2ed0fde8dcca3135804ce3f2c
mdrgoncalves <mdr.rgon@gmail.com>
2022-10-26 01:50:17 -0300

    storybook: menu item component

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -9,5 +9,6 @@ module.exports = {
     "@storybook/addon-interactions",
     "@storybook/addon-storysource"
   ],
+  staticDirs: ['../public'],
   "framework": "@storybook/html"
 }
---
+++ src/components/MenuItem.stories.ts
@@ -0,0 +1,65 @@
+import { Story, Meta } from '@storybook/html';
+import { createMenuItem, MenuItemProps } from './MenuItem';
+
+export default {
+    title: 'Components/Menu Item',
+    argTypes: {
+        iconType: { 
+            control: { type: 'select' },
+            options: ['home', 'explore', 'notifications', 'messages', 'bookmarks', 'lists', 'profile', 'more'],
+        },
+        label: { control: 'text' },
+    },
+} as Meta;
+
+const Template: Story<MenuItemProps> = (args) => {
+    return createMenuItem(args);
+};
+
+export const Home = Template.bind({});
+Home.args = {
+    iconType: 'home',
+    label: 'Home',
+};
+
+export const Explore = Template.bind({});
+Explore.args = {
+    iconType: 'explore',
+    label: 'Explore',
+};
+
+export const Notifications = Template.bind({});
+Notifications.args = {
+    iconType: 'notifications',
+    label: 'Notifications',
+};
+
+export const Messages = Template.bind({});
+Messages.args = {
+    iconType: 'messages',
+    label: 'Messages',
+};
+
+export const Bookmarks = Template.bind({});
+Bookmarks.args = {
+    iconType: 'bookmarks',
+    label: 'Bookmarks',
+};
+
+export const Lists = Template.bind({});
+Lists.args = {
+    iconType: 'lists',
+    label: 'Lists',
+};
+
+export const Profile = Template.bind({});
+Profile.args = {
+    iconType: 'profile',
+    label: 'Profile',
+};
+
+export const More = Template.bind({});
+More.args = {
+    iconType: 'more',
+    label: 'More',
+};
