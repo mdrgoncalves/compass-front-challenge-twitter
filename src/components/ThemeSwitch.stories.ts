8def579175b33b8c97c8f169f622769717cf69c5
mdrgoncalves <mdr.rgon@gmail.com>
2022-10-26 17:34:22 -0300

    feat: theme switch component

---
+++ src/components/ThemeSwitch.stories.ts
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/html';
+import { createThemeSwitch } from './ThemeSwitch';
+
+export default {
+    title: 'Components/Theme Switch',
+    argTypes: {
+        onClick: { action: 'clicked' }
+    },
+} as Meta;
+
+const Template: Story = () => {
+    return createThemeSwitch({
+        onClick: () => {
+            alert('Theme switch clicked');
+        }
+    });
+}
+
+export const Default = Template.bind({});
+Default.args = {};
