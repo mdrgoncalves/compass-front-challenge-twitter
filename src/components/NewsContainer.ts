e8d7cb8e9ac9253ef3e3dd3ded8af579ea12e9c2
mdrgoncalves <mdr.rgon@gmail.com>
2022-10-28 13:40:32 -0300

    feat: news container component

---
+++ src/components/NewsContainer.ts
@@ -0,0 +1,37 @@
+import '/public/assets/styles/main.css';
+import { createContentContainer } from './ContentContainer';
+import { createNewsCard } from './NewsCard';
+
+export const createNewsContainer = () => {
+
+    const firstNews = createNewsCard({
+        topic: 'COVID19',
+        topicDay: 'Last night',
+        newsTitleText: 'England’s Chief Medical Officer says the UK is at the most dangerous time of the pandemic',
+        newsImageLocation: '/assets/img/england-news.png',
+        hashText: 'covid19',
+    });
+
+    const secondNews = createNewsCard({
+        topic: 'US news',
+        topicDay: '4h ago',
+        newsTitleText: 'Parler may go offline following suspensions by Amazon, Apple and Google',
+        newsImageLocation: '/assets/img/us-news.png',
+        hashText: 'trump',
+    });
+
+    const thirdNews = createNewsCard({
+        topic: 'India',
+        topicDay: '1h ago',
+        newsTitleText: 'India vs Australia: India hold on to earn a draw on Day 5 in Sydney Test',
+        newsImageLocation: '/assets/img/india-news.png',
+        hashText: 'sport',
+    });
+
+    const newsContainer = createContentContainer({
+        contentTitle: 'What\'s happening',
+        components: [firstNews, secondNews, thirdNews]
+    });
+
+    return newsContainer;
+}
