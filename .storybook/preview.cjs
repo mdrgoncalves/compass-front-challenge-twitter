e192751aa3cccd3cb362608a8371e4c9f59ee553
mdrgoncalves <mdr.rgon@gmail.com>
2022-10-31 16:31:59 -0300

    style: small fixes to styling and storybook

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,4 +1,11 @@
 export const parameters = {
+  options: {
+    storySort: {
+        method: 'alphabetical',
+        order: ['Docs', ['Introduction', 'Colors'], 'Atoms', 'Molecules', 'Organisms', 'Pages'],
+        locales: 'en-US',
+    }
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
